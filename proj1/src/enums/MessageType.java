94c7314a70bcd8d1c70a079dc380ad6e682dbe37
Tiago Silva <tiagodusilva@gmail.com>
2021-03-25 20:13:47 +0000

    Added Message class to parse incoming messages

---
+++ proj1/src/enums/MessageType.java
@@ -0,0 +1,14 @@
+package enums;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum MessageType {
+    PUTCHUNK,
+    STORED,
+    GETCHUNK,
+    CHUNK,
+    DELETE,
+    REMOVED
+}
